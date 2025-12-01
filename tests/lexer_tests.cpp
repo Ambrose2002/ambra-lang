eaab9414bd0702416d24efdb098422e8c5320728
Ambrose Blay <ab2838@cornell.edu>
2025-11-30 19:13:57 -0500

    add single token test for minus

--- tests/lexer_tests.cpp
+++ tests/lexer_tests.cpp
@@ -42,3 +42,18 @@ TEST(SingleToken, Plus)
 
     ASSERT_TRUE(equalTokenVectors(actual, expected));
 }
+
+
+TEST(SingleToken, Minus)
+{
+    Token              token("-", PLUS, std::monostate{}, 1, 1);
+    Token              eof_token("", EOF_TOKEN, std::monostate{}, 1, 2);
+    std::vector<Token> actual = {token, eof_token};
+
+    std::string source = "";
+    Lexer       lexer(source);
+
+    std::vector<Token> expected = lexer.scanTokens();
+
+    ASSERT_TRUE(equalTokenVectors(actual, expected));
+}
