ae8ccbe074295f12aecd85a7ed997f504445f1e0
Ambrose Blay <ab2838@cornell.edu>
2025-11-30 19:14:57 -0500

    add single token test for right and left brace

--- tests/lexer_tests.cpp
+++ tests/lexer_tests.cpp
@@ -86,3 +86,33 @@ TEST(SingleToken, RightParen)
 
     ASSERT_TRUE(equalTokenVectors(actual, expected));
 }
+
+
+TEST(SingleToken, LeftBrace)
+{
+    Token              token("{", PLUS, std::monostate{}, 1, 1);
+    Token              eof_token("", EOF_TOKEN, std::monostate{}, 1, 2);
+    std::vector<Token> actual = {token, eof_token};
+
+    std::string source = "{";
+    Lexer       lexer(source);
+
+    std::vector<Token> expected = lexer.scanTokens();
+
+    ASSERT_TRUE(equalTokenVectors(actual, expected));
+}
+
+TEST(SingleToken, RightBrace)
+{
+    Token              token("}", PLUS, std::monostate{}, 1, 1);
+    Token              eof_token("", EOF_TOKEN, std::monostate{}, 1, 2);
+    std::vector<Token> actual = {token, eof_token};
+
+    std::string source = "}";
+    Lexer       lexer(source);
+
+    std::vector<Token> expected = lexer.scanTokens();
+
+    ASSERT_TRUE(equalTokenVectors(actual, expected));
+}
+
