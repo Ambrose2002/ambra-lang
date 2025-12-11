aa72c49932540f65dd9c8adc274dc09f03072246
Ambrose Blay <ab2838@cornell.edu>
2025-12-10 23:44:50 -0500

    implement binary and unary op kinds

--- src/ast/expr.h
+++ src/ast/expr.h
@@ -14,6 +14,24 @@ enum ExprKind
     Grouping
 };
 
+enum UnaryOpKind {
+    LogicalNot,
+    ArithmeticNegate
+};
+
+enum BinaryOpKind {
+    Equal,
+    NotEqual,
+    Less,
+    LessEqual,
+    Greater,
+    GreaterEqual,
+    Add,
+    Subtract,
+    Multiply,
+    Divide
+};
+
 class SourceLoc
 {
 
@@ -33,19 +51,49 @@ class Expr
 
 class IntLiteralExpr: public Expr {
 
+    public:
+    IntLiteralExpr (int value, SourceLoc loc): value(value), loc(loc) {};
+
     private:
     int value;
     SourceLoc loc;
 };
 
 class BoolLiteral: public Expr {
+
+    public:
+    BoolLiteral (bool value, SourceLoc loc): value(value), loc(loc) {};
+
     private:
     bool value;
     SourceLoc loc;
 };
 
 class StringLiteral: public Expr {
+
+    public:
+    StringLiteral (std::string value, SourceLoc loc): value(value), loc(loc) {};
+
     private:
     std::string value;
     SourceLoc loc;
+};
+
+class VariableExpr: public Expr {
+
+    public:
+    VariableExpr (std::string name, SourceLoc loc): name(name), loc(loc) {};
+
+    private:
+    std::string name;
+    SourceLoc loc;
+};
+
+class UnaryExpr: public Expr {
+    public:
+    UnaryExpr ();
+
+    private:
+    UnaryOpKind op;
+    Expr* operand;
 };
