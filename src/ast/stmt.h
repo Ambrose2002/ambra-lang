9666c90306d8ff2dee0f99995e8f6b0e08b3d2f4
Ambrose Blay <ab2838@cornell.edu>
2025-12-11 18:33:46 -0500

    add stmt variants

--- src/ast/stmt.h
+++ src/ast/stmt.h
@@ -1,4 +1,12 @@
-enum StmtKind {
+#include "expr.h"
+
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <vector>
+
+enum StmtKind
+{
     Summon,
     Say,
     Block,
@@ -6,29 +14,46 @@ enum StmtKind {
     While
 };
 
-class Stmt {
-    public:
+class Stmt
+{
+  public:
     StmtKind kind;
     virtual ~Stmt() {};
 };
 
+class SummonSmt : public Stmt
+{
 
-class SummonSmt: public Stmt {
+  public:
+    SummonSmt(std::string name, std::unique_ptr<Expr> initializer, SourceLoc loc)
+        : name(name), initializer(std::move(initializer)), loc(loc)
+    {
+    }
 
+  private:
+    std::string           name;
+    std::unique_ptr<Expr> initializer;
+    SourceLoc             loc;
 };
 
-class SayStmt: public Stmt {
-
+class SayStmt : public Stmt
+{
+  private:
+    std::unique_ptr<Expr> expression;
+    SourceLoc             loc;
 };
 
-class BlockStmt: public Stmt {
-
+class BlockStmt : public Stmt
+{
+  private:
+    std::vector<Stmt> statements;
+    SourceLoc         loc;
 };
 
-class IfChainStmt: public Stmt {
-
+class IfChainStmt : public Stmt
+{
 };
 
-class WhileStmt: public Stmt {
-
+class WhileStmt : public Stmt
+{
 };
