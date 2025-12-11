2a382f99174d3be7ec6cbc7c35fcf304836f7bf0
Ambrose Blay <ab2838@cornell.edu>
2025-12-10 23:32:13 -0500

    define expr helper types

--- src/ast/expr.h
+++ src/ast/expr.h
@@ -1,5 +1,36 @@
 
+enum ExprKind
+{
+    IntLiteral,
+    BoolLiteral,
+    StringLIteral,
+    InterpolatedString,
+    Variable,
+    Unary,
+    Binary,
+    Grouping
+};
 
-class IntLiteral {
-    
+class SourceLoc
+{
+
+  public:
+    SourceLoc(int line, int col) : line(line), col(col) {};
+    int line;
+    int col;
+};
+
+
+class StringPart {
+
+};
+class Expr
+{
+};
+
+class IntLiteralExpr: public Expr {
+
+    private:
+    int value;
+    SourceLoc loc;
 };
