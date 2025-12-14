179779921df248b64b3027f0a4e14d6a16dbfce0
Ambrose Blay <ab2838@cornell.edu>
2025-12-13 22:45:04 -0500

    add public constructor

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -1,5 +1,11 @@
 #include "ast/stmt.h"
 #include "ast/expr.h"
+#include "lexer/lexer.h"
+#include <vector>
 class Parser {
+    public:
+    Parser(std::vector<Token> tokens): tokens(tokens){};
 
+    private:
+    std::vector<Token> tokens;
 };
