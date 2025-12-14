ee7fde23083f4ebf93f8d89942c8773c59de0b71
Ambrose Blay <ab2838@cornell.edu>
2025-12-13 23:23:38 -0500

    add parser navigation helpers

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -1,16 +1,28 @@
-#include "ast/stmt.h"
 #include "ast/expr.h"
+#include "ast/stmt.h"
 #include "lexer/lexer.h"
+
+#include <string>
 #include <vector>
-class Parser {
-    public:
-    Parser(const std::vector<Token>& tokens): tokens(tokens){};
+class Parser
+{
+  public:
+    Parser(const std::vector<Token>& tokens) : tokens(tokens) {};
 
     void parseExpression();
 
-    bool hasError();
+    bool hadError();
 
-    private:
+  private:
     const std::vector<Token>& tokens;
-    size_t current;
+    size_t                    current;
+
+    Token peek();
+    Token previous();
+    bool  isAtEnd();
+    Token advance();
+    bool  check(TokenType t);
+    bool  match();
+    void  consume(TokenType t, std::string msg);
+    void  reportError(const Token& where, const std::string& message);
 };
