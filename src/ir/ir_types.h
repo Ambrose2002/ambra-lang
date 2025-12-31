7508544b65875da248e018677c9e91935374b408
Ambrose Blay <ab2838@cornell.edu>
2025-12-31 15:15:53 -0500

    implement ir types

---
+++ src/ir/ir_types.h
@@ -0,0 +1,32 @@
+#include "ast/expr.h"
+
+#include <cstdint>
+#include <string>
+#include <variant>
+
+enum IrType
+{
+    I32,
+    Bool,
+    String,
+    Void
+};
+
+using LocalId = uint32_t;
+using LabelId = uint32_t;
+using ConstId = uint32_t;
+
+struct LocalInfo
+{
+    LocalId     id;
+    IrType      type;
+    std::string debugName;
+    SourceLoc   declLoc;
+};
+
+struct Constant
+{
+    IrType type;
+    ConstId constId;
+    std::variant<std::string, int, bool> value;
+};
