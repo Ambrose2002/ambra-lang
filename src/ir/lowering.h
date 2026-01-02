2e58fec9410632a00ebdecb187819b067091a842
Ambrose Blay <ab2838@cornell.edu>
2026-01-02 10:48:15 -0500

    implement identifier expression lowering

--- src/ir/lowering.h
+++ src/ir/lowering.h
@@ -1,14 +1,13 @@
 #include "program.h"
 #include "sema/analyzer.h"
 
-#include <stack>
 #include <unordered_map>
 struct LoweringContext
 {
     IrFunction* currentFunction;
     IrProgram*  program;
 
-    std::stack<std::unordered_map<const Symbol*, LocalId>> localScopes;
+    std::vector<std::unordered_map<const Symbol*, LocalId>> localScopes;
 
     const TypeTable&       typeTable;
     const ResolutionTable& resolutionTable;
