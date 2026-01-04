679e11623417594b16695b4124601a80d445d5fc
Ambrose Blay <ab2838@cornell.edu>
2026-01-03 21:26:39 -0500

    implement instruction validation

--- src/ir/validator.h
+++ src/ir/validator.h
@@ -164,7 +164,37 @@ struct IrValidator
         }
     }
 
-    void validateInstruction(size_t ip, std::vector<IrType> stack)
+    bool maintainStack(size_t popCount, size_t pushCount, IrType popType, IrType pushType,
+                       std::vector<IrType>& stack, size_t ip)
+    {
+        if (stack.size() < popCount)
+        {
+            diagnostics.push_back({"Stack underflow", ip});
+            return false;
+        }
+
+        for (size_t i = 0; i < popCount; i++)
+        {
+            if (stack.back() != popType)
+            {
+                diagnostics.push_back({std::string("type mismatch: expected") +
+                                           getTypeName(popType) + (", got") +
+                                           getTypeName(stack.back()),
+                                       ip});
+                return false;
+            }
+            stack.pop_back();
+        }
+
+        for (size_t i = 0; i < pushCount; i++)
+        {
+            stack.push_back(pushType);
+        }
+
+        return true;
+    }
+
+    void validateInstruction(size_t ip, std::vector<IrType>& stack)
     {
         switch (function.instructions[ip].opcode)
         {
@@ -173,18 +203,155 @@ struct IrValidator
         case MulI32:
         case DivI32:
         {
-            if (stack.size() < 2)
+            bool res = maintainStack(2, 1, I32, I32, stack, ip);
+            if (!res)
+                return;
+            break;
+        }
+        case NotBool:
+        {
+            bool res = maintainStack(1, 1, Bool32, Bool32, stack, ip);
+            if (!res)
+                return;
+            break;
+        }
+        case NegI32:
+        {
+            bool res = maintainStack(1, 1, I32, I32, stack, ip);
+            if (!res)
+                return;
+            break;
+        }
+        case CmpEqI32:
+        case CmpNEqI32:
+        case CmpLtI32:
+        case CmpLtEqI32:
+        case CmpGtI32:
+        case CmpGtEqI32:
+        {
+            bool res = maintainStack(2, 1, I32, Bool32, stack, ip);
+            if (!res)
+                return;
+            break;
+        }
+        case CmpEqBool32:
+        case CmpNEqBool32:
+        {
+            bool res = maintainStack(2, 1, Bool32, Bool32, stack, ip);
+            if (!res)
+                return;
+            break;
+        }
+        case CmpEqString32:
+        case CmpNEqString32:
+        {
+            bool res = maintainStack(2, 1, String32, Bool32, stack, ip);
+            if (!res)
+                return;
+            break;
+        }
+        case LoadLocal:
+        {
+            LocalId lid = std::get<LocalId>(function.instructions[ip].operand);
+
+            if (lid.value >= function.localTable.locals.size())
+            {
+                diagnostics.push_back({"Invalid LocalId", ip});
+                return;
+            }
+
+            stack.push_back(function.localTable.locals[lid.value].type);
+            break;
+        }
+        case StoreLocal:
+        {
+            LocalId lid = std::get<LocalId>(function.instructions[ip].operand);
+
+            if (lid.value >= function.localTable.locals.size())
+            {
+                diagnostics.push_back({"Invalid LocalId", ip});
+                return;
+            }
+
+            if (stack.empty())
             {
                 diagnostics.push_back({"Stack underflow", ip});
                 return;
             }
-            if (stack.back() != I32 && stack[stack.size() - 2])
+
+            IrType expected = function.localTable.locals[lid.value].type;
+            IrType actual = stack.back();
+
+            if (actual != expected)
             {
-                diagnostics.push_back(
-                    {std::string("type mismatch: expected I32, got") + getTypeName(stack.back()),
-                     ip});
+                diagnostics.push_back({std::string("type mismatch: expected ") +
+                                           getTypeName(expected) + ", got " + getTypeName(actual),
+                                       ip});
+                return;
             }
+
+            stack.pop_back();
+            break;
         }
+        case PushConst:
+        {
+            ConstId cid = std::get<ConstId>(function.instructions[ip].operand);
+
+            if (cid.value >= program.constants.size())
+            {
+                diagnostics.push_back({"Invalid ConstId", ip});
+                return;
+            }
+
+            stack.push_back(program.constants[cid.value].type);
+            break;
+        }
+        case ToString:
+        {
+            if (!stack.size())
+            {
+                diagnostics.push_back({"Stack underflow", ip});
+                return;
+            }
+
+            if (stack.back() == Void32)
+            {
+                diagnostics.push_back({std::string("type mismatch: expected ") +
+                                           getTypeName(String32) + " or " + getTypeName(I32) +
+                                           " or " + getTypeName(Bool32) + (", got") +
+                                           getTypeName(stack.back()),
+                                       ip});
+                return;
+            }
+            stack.pop_back();
+            stack.push_back(String32);
+            break;
+        }
+        case ConcatString:
+        {
+            bool res = maintainStack(2, 1, String32, String32, stack, ip);
+            if (!res)
+                return;
+            break;
+        }
+        case PrintString:
+        {
+            bool res = maintainStack(1, 0, String32, Void32, stack, ip);
+            if (!res)
+                return;
+            break;
+        }
+        case JumpIfFalse:
+        {
+            bool res = maintainStack(1, 0, Bool32, Void32, stack, ip);
+            if (!res)
+                return;
+            break;
+        }
+        case Jump:
+        case JLabel:
+        case Nop:
+            break;
         default:
             diagnostics.push_back({"Unexpected opcode", ip});
         }
