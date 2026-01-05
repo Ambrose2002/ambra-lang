16326ff2e43adf9d8f750bcecd872006f054e1fd
Ambrose Blay <ab2838@cornell.edu>
2026-01-04 21:32:23 -0500

    implement vm type

---
+++ src/vm/vm.h
@@ -0,0 +1,34 @@
+#include "ir/validator.h"
+
+#include <cstddef>
+#include <variant>
+
+struct Value
+{
+    IrType                               tag;
+    std::variant<std::string, int, bool> payload;
+};
+
+struct VM
+{
+    // Execution state
+    size_t ip = 0;
+
+    // Operand stack
+    std::vector<Value> stack;
+
+    // Locals (indexed by LocalId)
+    std::vector<Value> locals;
+
+    // IR references (no ownership)
+    const IrProgram&  program;
+    const IrFunction& function;
+
+    // Constructor initializes locals from function metadata
+    VM(const IrProgram& prog, const IrFunction& func) : program(prog), function(func)
+    {
+        locals.resize(function.localTable.locals.size());
+    }
+
+    
+};
